3dca8e8051842a1b089fe4243960d591ee3e5190
Katy Daviscourt and Lawrence Eby <katy+lawrence@>
2017-03-01 15:21:57 -0800

    Add restful routing for book and author

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -13,6 +13,59 @@ namespace Library
                 model.Add("Overdue", Checkout.GetAllOverdue("2016-01-01"));
                 return View["index.cshtml", model];
             };
+
+            Post["/add-book"] = _ => {
+                Book newBook = new Book(Request.Form["book-name"], Request.Form["copies"]);
+                newBook.Save();
+
+                Dictionary<string, object> model = ModelMaker();
+                model.Add("Book", newBook);
+
+                return View["book.cshtml", model];
+            };
+
+            Post["/add-author"] = _ => {
+                Author newAuthor = new Author(Request.Form["author-name"]);
+                newAuthor.Save();
+
+                Dictionary<string, object> model = ModelMaker();
+                model.Add("Author", newAuthor);
+
+                return View["author.cshtml", model];
+            };
+
+
+            Get["/author/{id}"] = parameters => {
+                Dictionary<string, object> model = ModelMaker();
+                model.Add("Author", Author.Find(parameters.id));
+
+                return View["author.cshtml", model];
+            };
+
+            Post["/author/{id}"] = parameters => {
+                Author.Find(parameters.id).AddBook(Request.Form["book-id"]);
+
+                Dictionary<string, object> model = ModelMaker();
+                model.Add("Author", Author.Find(parameters.id));
+
+                return View["author.cshtml", model];
+            };
+
+            Get["/book/{id}"] = parameters => {
+                Dictionary<string, object> model = ModelMaker();
+                model.Add("Book", Book.Find(parameters.id));
+
+                return View["book.cshtml", model];
+            };
+
+            Post["/book/{id}"] = parameters => {
+                Book.Find(parameters.id).AddAuthor(Request.Form["author-id"]);
+
+                Dictionary<string, object> model = ModelMaker();
+                model.Add("Book", Book.Find(parameters.id));
+
+                return View["book.cshtml", model];
+            };
         }
 
         public Dictionary<string, object> ModelMaker()
